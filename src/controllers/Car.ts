50a6219108e4fa848fed5990caa6353b8abc2e00
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-21 19:31:10 -0300

    :sparkles: feat: class controller do metodo post na rota Car

---
+++ src/controllers/Car.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { ICar } from '../interfaces/ICar';
+import IService from '../interfaces/IService';
+import 'express-async-errors';
+
+class CarController {
+  readonly _service: IService<ICar>;
+  constructor(service:IService<ICar>) {
+    this._service = service;
+  }
+
+  create = async (req:Request, res:Response<ICar>, _next:NextFunction) => {
+    const response = await this._service.create(req.body);
+    res.status(201).json(response);
+  };
+}
+export default CarController;
