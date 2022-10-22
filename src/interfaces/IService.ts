e67deaf03a7e42628731eca6c924477ca076f6aa
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-22 11:29:16 -0300

    :sparkles: feat: implementado o metodo readOne na classe Car em Service

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,4 +1,5 @@
 export default interface IService<T> {
   create(obj:unknown):Promise<T>
   read():Promise<T[]>
+  readOne(_id: string):Promise<T>
 }
--- src/services/Car.ts
+++ src/services/Car.ts
@@ -1,12 +1,19 @@
 import { ICar, CarZodSchema } from '../interfaces/ICar';
 import IService from '../interfaces/IService';
 import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
 
 class CarService implements IService<ICar> {
   readonly _model : IModel<ICar>;
 
   constructor(model:IModel<ICar>) {
     this._model = model;
+  }
+
+  async readOne(_id:string):Promise<ICar> {
+    const car = await this._model.readOne(_id);
+    if (!car) throw new Error(ErrorTypes.NotFound);
+    return car;
   }
 
   async read():Promise<ICar[]> {
