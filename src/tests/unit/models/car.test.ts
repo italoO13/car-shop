471bbb1a4d4ff61af5d6df688cf9dce294cf58d1
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-22 11:20:53 -0300

    :test_tube: test: motodo get da rota /cars/id

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -2,8 +2,9 @@ import { expect } from "chai";
 import sinon from 'sinon';
 import { Model } from "mongoose";
 import CarModel from "../../../models/Car";
+import  Mongoose from "mongoose";
 import {carMock, carMockWithId} from '../../mocks/car'
-
+import {ErrorTypes} from '../../../errors/catalog'
 
 describe('Rota de model', () => {
   const carModel = new CarModel()
@@ -11,8 +12,15 @@ describe('Rota de model', () => {
   before(() => {
     sinon.stub(Model, 'create').resolves(carMockWithId)
     sinon.stub(Model, 'find')
-    .onFirstCall().resolves([carMockWithId, carMockWithId])
-    .onSecondCall().resolves([])
+      .onFirstCall().resolves([carMockWithId, carMockWithId])
+      .onSecondCall().resolves([])
+    sinon.stub(Model, 'findById')
+      .onFirstCall().resolves(carMockWithId)
+      .resolves(null)
+    sinon.stub(Mongoose, 'isValidObjectId')
+      .onFirstCall().returns(false)
+      .resolves(true)
+
   })
 
   after(() => {
@@ -36,6 +44,29 @@ describe('Rota de model', () => {
       it('deve retornar um array vazio caso não seja encontrado nenhum car', async() => {
         const readCar = await carModel.read()
         expect(readCar).to.be.deep.equal([])
+      })
+
+    })
+
+    describe('quando a rota é "/cars/id"', () => {
+      it('quando o id é invalido deve retornar erro do type InvalidId', async() => {
+        let error:any;
+        try {
+          await carModel.readOne('invalidId');
+        } catch (e) {
+          error = e;
+        }
+        expect(error.message).to.be.eq(ErrorTypes.InvalidId)
+      })
+
+      it ('deve retornar um objeto de cars caso seja encontrado', async () => {
+        const readone = await carModel.readOne('idValid');
+        expect(readone).to.be.deep.eq(carMockWithId);
+      })
+
+      it ('Deve retornar null caso não seja encontrado nenhum objeto', async () => {
+        const readone = await carModel.readOne('idValid');
+        expect(readone).to.be.deep.eq(null);
       })
 
     })
