dae264799ada572b1d95067e3250c1b398cf757e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-22 17:11:49 -0300

    :sparkles: feat: interface IMotorcycle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const categoryCar = ['Street', 'Custom', 'Trail'] as const;
+
+const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(categoryCar),
+  engineCapacity: z.number().int().min(0).max(2500),
+});
+
+type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+
+export {
+  MotorcycleZodSchema, 
+  IMotorcycle,
+};
