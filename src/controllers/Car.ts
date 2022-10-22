08c4972a3315db4841489a64778f998c633d119b
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-22 14:45:26 -0300

    :sparkles: feat: implementado o metodo update na classe Car em controller

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -9,6 +9,12 @@ class CarController {
     this._service = service;
   }
 
+  update = async (req:Request, res:Response<ICar>, _next:NextFunction) => {
+    const { id } = req.params;
+    const response = await this._service.update(id, req.body);
+    res.status(200).json(response);
+  };
+
   create = async (req:Request, res:Response<ICar>, _next:NextFunction) => {
     const response = await this._service.create(req.body);
     res.status(201).json(response);
