3e796e4701a2dcf02030e92f92908403ef2af3ad
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-23 11:42:25 -0300

    fix: alterado minimo aceitavem na propriedade engineCapacity

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -5,7 +5,7 @@ const categoryCar = ['Street', 'Custom', 'Trail'] as const;
 
 const MotorcycleZodSchema = VehicleZodSchema.extend({
   category: z.enum(categoryCar),
-  engineCapacity: z.number().int().min(0).max(2500),
+  engineCapacity: z.number().int().min(1).max(2500),
 });
 
 type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
